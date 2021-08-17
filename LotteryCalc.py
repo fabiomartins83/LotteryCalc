529688fe8d997d471740e3a93a8646d67c60e7e5
Fabio de Almeida Martins <fabio.martins.01@hotmail.com>
2021-08-17 11:56:28 -0300

    adicionando menu de opções ao projeto

--- LotteryCalc.py
+++ LotteryCalc.py
@@ -26,29 +26,65 @@ def coeficiente(n1,n2,n3,n4):
     result = coef1*coef2
     return result
 
-sair = False
-while (sair == False):
-    os.system("clear")
-    print("CALCULADORA LOTERICA\n")
+def opcao1():
+    global jogo, n1, n2, n4
+    jogo = "MEGASENA"
+    n1 = int(60)
+    n2 = int(6)
+    n4 = int(4)
+    
+def opcao2():
+    global jogo, n1, n2, n4
+    jogo = "QUINA"
+    n1 = int(80)
+    n2 = int(5)
+    n4 = int(2)
+    
+def opcao3():
+    global jogo, n1, n2, n4
+    jogo = "LOTOFACIL"
+    n1 = int(25)
+    n2 = int(15)
+    n4 = int(12)
+    
+def default():
+    global n1, n2, n4
     n1 = 0
     while ((n1 <= 0) | (n1 == "")):
         n1 = int(input("Insira a quantidade total de dezenas disponíveis: "))
     n2 = n1+1
     while ((n2 >= n1) | (n2 <= 0) | (n2 == "")):
         n2 = int(input("Insira a quantidade de dezenas sorteadas: "))
-    n3 = n1+1
-    while ((n3 > n1) | (n3 <= 0) | (n3 == "")):
-        n3 = int(input("Insira a quantidade de dezenas apostadas: "))
     n4 = n2+1
     while ((n4 > n2) | (n4 < 0) | (n4 == "")):
         n4 = int(input("Insira o número de dezenas acertadas para premiação mínima: "))
-    print("\nSORTEIO DE",int(n2),"dezenas de 01 a",int(n1),":")
+
+sair = False
+while (sair == False):
+    print("CALCULADORA LOTERICA\n")
+    print("1 - Megasena")
+    print("2 - Quina")
+    print("3 - Lotofácil")
+    switch = {
+        "1": opcao1,
+        "2": opcao2,
+        "3": opcao3
+    }
+    jogo = "não definido"
+    case = switch.get(input("\nDigite uma opcao (1, 2 ou 3): "), default)
+    os.system("clear")
+    case()
+    n3 = n1+1
+    while ((n3 > n1) | (n3 <= n4) | (n3 == "")):
+        n3 = int(input("Insira a quantidade de dezenas apostadas: "))
+    print("\nCalculando probabilidade para ",jogo)
+    print("Sorteio de",int(n2),"dezenas de 01 a",int(n1),":")
     for j in range(n2,n3+1,1):
         print("\nAposta de",int(j),"DEZENAS: ")
         for i in range(n2,n4-1,-1):
             fator = combinacao(n1,n2)
             coef = coeficiente(n1,n2,j,i)
             result = fator / coef
-            print(int(i),"acertos = 1 /",round(result,1),"\n  (coeficiente =",int(coef),"x)")
-    if (input("Deseja sair? (S/N)").lower() == "s"):
+            print(int(i),"acertos = (1 /",round(result,1),")        coeficiente =",int(coef),"x")	
+    if (input("\nDeseja sair? (S/N)").lower() == "s"):
         sair = True
